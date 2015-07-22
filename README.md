f0cfc057634c4be7dcd1771affb9fb4750220c50
mika <mail@deadcells.org>
2015-07-22 16:50:06 +0200

    Added support for transparency
    Now factors in the alpha channel in case a transparent image is given

--- README.md
+++ README.md
@@ -11,5 +11,4 @@ Port of my crystalliser processing sketch to python to run as a [twitter bot](ht
 
 - Instead of saving output image to disk, just make an in memory object
 - Implement a form of Last Seen ID, to pull mentions past a certain tweet id (no repeats)
-- Test with images with transparency - do transparent pixels wash out surrounding ones?
 - Incorporate .gif functionality
--- crystalliser.py
+++ crystalliser.py
@@ -30,36 +30,36 @@ def crystallise(image):
     return image
 
 def drawTopHalf(xOrigin, yOrigin, pixelMap):
-    rgb = [0,0,0]
+    rgba = [0,0,0,0]
     counter = 0
     for y in range(0,chunkSize-1,4):
         for x in range(0,(chunkSize - 1 - y) - (y % 2),4):
             pixel = pixelMap[xOrigin+x,yOrigin+y]
-            rgb = [sum(val) for val in zip(rgb,pixel)]
+            rgba = [sum(val) for val in zip(rgba,pixel)]
             counter+= 1
 
-    averageChunkRGB = [val / counter for val in rgb]
+    averageChunkRGBA = [val / counter for val in rgba]
 
     #Change all the pixels in the group to this colour
     for y in range(chunkSize-1):
         for x in range((chunkSize - 1 - y) - (y % 2)):
-            pixelMap[xOrigin+x,yOrigin+y] = tuple(averageChunkRGB)
+            pixelMap[xOrigin+x,yOrigin+y] = tuple(averageChunkRGBA)
 
 def drawBottomHalf(xOrigin, yOrigin, pixelMap):
-    rgb = [0,0,0]
+    rgba = [0,0,0,0]
     counter = 0
     for y in range(1,chunkSize,4):
         for x in range(((chunkSize - 1 - y) + (y+1)%2),chunkSize,4):
             pixel = pixelMap[xOrigin+x,yOrigin+y]
-            rgb = [sum(val) for val in zip(rgb,pixel)]
+            rgba = [sum(val) for val in zip(rgba,pixel)]
             counter += 1
 
-    averageChunkRGB = [val / counter for val in rgb]
+    averageChunkRGBA = [val / counter for val in rgba]
 
     #Change all pixels in the group to this colour
     for y in range(1,chunkSize):
         for x in range(((chunkSize - 1 - y) + (y+1)%2),chunkSize):
-            pixelMap[xOrigin+x,yOrigin+y] = tuple(averageChunkRGB)
+            pixelMap[xOrigin+x,yOrigin+y] = tuple(averageChunkRGBA)
 
 
 ## Twitter API stuff
